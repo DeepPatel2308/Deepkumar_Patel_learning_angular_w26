f503adcbb21428d0de7c1ac99a25c57bed800216
deepp <w0878389@myscc.ca>
2026-01-20 11:08:50 -0500

    Initial Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,7 +12,7 @@ import {NgForOf} from '@angular/common';
 export class App {
   title:string = 'Deepkumar Patel';
   //initializing an array with at least 6 instances of the object
-  reataurantManuList: Restaurant[] = [
+  restaurantMenuList: Restaurant[] = [
     {Name: "Manchurian", Price: 12, Category: "Starter", Quantity: 250, Description: "Prepared with tomato pury."},
     {Name: "Paneer Tikka", Price: 18, Category: "Starter", Quantity: 200, Description: "Prepared with migrated cubes of paneer."},
     {Name: "Paneer Butter Masala", Price: 23, Category: "Main Course", Quantity: 280, Description: "Prepared with gray with paneer."},
