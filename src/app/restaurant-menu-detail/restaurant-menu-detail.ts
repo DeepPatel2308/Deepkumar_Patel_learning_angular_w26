9852d4870a3731535251bddc52530d468c683653
Deep Patel <w0878389@myscc.ca>
2026-01-27 20:28:05 -0500

    component receives the restaurant item and index via @input

--- src/app/restaurant-menu-detail/restaurant-menu-detail.ts
+++ src/app/restaurant-menu-detail/restaurant-menu-detail.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Restaurant } from '../models/restaurant.Menu';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-restaurant-menu-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './restaurant-menu-detail.html',
   styleUrl: './restaurant-menu-detail.css',
 })
 export class RestaurantMenuDetail {
-
+  @Input() restaurant?:Restaurant;
+  @Input() index?: number = 0;
 }
