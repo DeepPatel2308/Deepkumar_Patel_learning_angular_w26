81410062769c169bb659b421c447b8d96f582527
Deep Patel <w0878389@myscc.ca>
2026-02-06 13:18:46 -0500

    moving Icontent array to this file

--- src/app/data/mock-restaurant-menu.ts
+++ src/app/data/mock-restaurant-menu.ts
@@ -1,2 +1,10 @@
-export interface MockRestaurantMenu {
-}
+import {Restaurant} from '../models/restaurant.Menu';
+
+
+export const restaurantMenuList: Restaurant[] = [{Name: "Manchurian", Price: 12, Category: "Starter", Quantity: 250, Description: "Prepared with tomato pury."},
+{Name: "Paneer Tikka", Price: 18, Category: "Starter", Quantity: 200, Description: "Prepared with migrated cubes of paneer."},
+{Name: "Paneer Butter Masala", Price: 23, Category: "Main Course", Quantity: 280, Description: "Prepared with gray with paneer."},
+{Name: "Fries", Price: 16, Category: "Starter", Quantity: 300, Description: "Prepared with patato fries."},
+{Name: "Zira Rice", Price: 9, Category: "Main Course", Quantity: 300, Description: "Prepared with zira flavoured rice."}
+];
+
