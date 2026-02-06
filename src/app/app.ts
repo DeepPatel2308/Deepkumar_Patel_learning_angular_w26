736f83636cf18f1496d7d8b651a9c32198d15995
Deep Patel <w0878389@myscc.ca>
2026-02-06 14:27:05 -0500

    adding id and changing version

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RestaurantMenuList } from './restaurant-menu-list/restaurant-menu-list';
+import {restaurantMenuList} from './data/mock-restaurant-menu';
+import {RestaurantMenuDetail} from './restaurant-menu-detail/restaurant-menu-detail';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RestaurantMenuList],
+  imports: [RestaurantMenuList, RestaurantMenuDetail],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   title: string = 'Deepkumar Patel';
+  protected readonly indexedDB = indexedDB;
+  protected readonly restaurantMenuList = restaurantMenuList;
 }
--- src/app/restaurant-menu-list/restaurant-menu-list.ts
+++ src/app/restaurant-menu-list/restaurant-menu-list.ts
@@ -11,11 +11,11 @@ import { Restaurant } from '../models/restaurant.Menu';
 export class RestaurantMenuList {
 
   restaurantMenuList: Restaurant[] = [
-    {Name: "Manchurian", Price: 12, Category: "Starter", Quantity: 250, Description: "Prepared with tomato pury."},
-    {Name: "Paneer Tikka", Price: 18, Category: "Starter", Quantity: 200, Description: "Prepared with migrated cubes of paneer."},
-    {Name: "Paneer Butter Masala", Price: 23, Category: "Main Course", Quantity: 280, Description: "Prepared with gray with paneer."},
-    {Name: "Fries", Price: 16, Category: "Starter", Quantity: 300, Description: "Prepared with patato fries."},
-    {Name: "Zira Rice", Price: 9, Category: "Main Course", Quantity: 300, Description: "Prepared with zira flavoured rice."}
+    {Id:1, Name: "Manchurian", Price: 12, Category: "Starter", Quantity: 250, Description: "Prepared with tomato pury."},
+    {Id:2, Name: "Paneer Tikka", Price: 18, Category: "Starter", Quantity: 200, Description: "Prepared with migrated cubes of paneer."},
+    {Id:3, Name: "Paneer Butter Masala", Price: 23, Category: "Main Course", Quantity: 280, Description: "Prepared with gray with paneer."},
+    {Id:4, Name: "Fries", Price: 16, Category: "Starter", Quantity: 300, Description: "Prepared with patato fries."},
+    {Id:5, Name: "Zira Rice", Price: 9, Category: "Main Course", Quantity: 300, Description: "Prepared with zira flavoured rice."}
   ];
 
 }
