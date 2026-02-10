e66a9ce1a5c77b18e3f5cd1358fa59153e8f17ef
Deep Patel <w0878389@myscc.ca>
2026-02-10 10:26:26 -0500

    adding the new components in as routes

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,13 @@ import { App } from './app/app';
 import {restaurantMenuList} from './app/data/mock-restaurant-menu';
 import {RestaurantMenuList} from './app/restaurant-menu-list/restaurant-menu-list';
 import {RestaurantMenuDetail} from './app/restaurant-menu-detail/restaurant-menu-detail';
+import {ModifyListItem} from './app/modify-list-item/modify-list-item';
+import {PageNotFound} from './app/page-not-found/page-not-found';
 
 const routes = [
+  { path:'', redirectTo: '/restaurant-menu', pathMatch:'full'},
   { path: 'restaurant-menu', component: RestaurantMenuList},
-  { path: 'reataurant-menu/:id', component: RestaurantMenuDetail}
+  { path: 'reataurant-menu/:id', component: RestaurantMenuDetail},
+  { path:'modify-list-item', component: ModifyListItem},
+  { path: '**', component:PageNotFound}
 ];
